6fa961f5702c075ddeda8f3350078e7e7658a24c
aki <errabas112@gmail.com>
2026-04-10 02:26:38 +0800

    Update viewport meta tag to prevent user scaling on mobile devices

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if (is_logged_in()) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>Citas Smart Archive Web System</title>
     <link rel="icon" type="image/png" href="img/CITAS_logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
