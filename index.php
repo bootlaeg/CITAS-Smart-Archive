0cbad1037132e73f8b85f4085ff67df340e8c5b0
aki <errabas112@gmail.com>
2026-04-11 19:49:49 +0800

    s

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (is_logged_in()) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>sCITAS Smart Archive</title>
+    <title> CITAS Smart Archive</title>
     <link rel="icon" type="image/png" href="img/CITAS_logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
